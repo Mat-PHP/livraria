e282a1e0f31fb3a93dc45a1d89e54cd0e003185e
Mateus Ferreira Salustiano <40248501801@>
2025-10-03 11:36:40 -0300

    Login

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { HomeComponent } from './pages/home/home.components';
 import { AutoresPage } from './pages/authors/authors.components';
 import { PublisherComponent } from './pages/publishe/publisher.component';
 import { BooksComponent } from './pages/books/books.components';
+import { LoginComponents } from './pages/login.components/login.components';
+import { authGuard } from './auth.guard';
 
 
 
 export const routes: Routes =[
-    {path: '', component: HomeComponent},
+    {path: '', component: LoginComponents},
+    {path: 'login', component: LoginComponents},
     {path: 'home', component: HomeComponent},
-    {path: 'autores',component: AutoresPage},
-    {path: 'editoras', component: PublisherComponent},
+    {path: 'autores',component: AutoresPage,canActivate:[authGuard]},
+    {path: 'editoras', component: PublisherComponent,canActivate: [authGuard]},
     {path: 'livros', component: BooksComponent}
 ];

20bda3364f7fc173ada51fc298ccbf8a57b1c342
Mateus Ferreira Salustiano <40248501801@>
2025-10-10 11:29:21 -0300

    imagens

--- front/src/app/services/imagens.services.ts
+++ front/src/app/services/imagens.services.ts
@@ -19,8 +19,18 @@ export class ImagemService{
         const token = localStorage.getItem('access')
         return token ? new HttpHeaders({Authorization: `Bearer ${token}`}) : new HttpHeaders()
     }
+
     listar(): Observable<Imagem[]>{
-        return this.http.get<Imagem[]>(this.base,{headers: this.headers()})
-        
+        return this.http.get<Imagem[]>(this.base, {headers: this.headers()})
     }
-}   
+    
+    enviar(file: File): Observable<Imagem>{
+        const form =new FormData()
+        form.append('imagem', file)
+        return this.http.post<Imagem>(this.base, form, {headers: this.headers()})
+    }
+
+    deletar(id: number){
+        return this.http.delete(`${this.base}${id}/`, {headers: this.headers()})
+    }
+}
